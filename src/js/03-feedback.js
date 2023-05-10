53806bf4ae3130a94c1a2cd1560f86ce37123d08
KateShepel <MariyaKaterina@gmail.com>
2023-05-10 11:00:59 +0300

    Додає ДЗ 8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input')
+}
+let formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onAllTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(localStorage.getItem('feedback-form-state'));
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+function populateTextarea() {
+    const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (savedMessage) {
+        refs.input.value = savedMessage.email;
+        refs.textarea.value = savedMessage.message;
+    }
+}
+
+function onAllTextareaInput(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    const priviosValue = localStorage.getItem('feedback-form-state');
+    const newValue = { [name]: value };
+    formData = {...JSON.parse(priviosValue), ...newValue};
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
